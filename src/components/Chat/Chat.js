37b6f49dd4643f1a3212645c29f52a8fcee98a38
juliantiu <jtiu.edu@gmail.com>
2020-02-08 17:00:35 -0600

    added a space to see how submodule works. Turns out, I cant commit the changes in client from parent

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from 'socket.io-client'; 
 
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
